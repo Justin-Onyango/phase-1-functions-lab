b230bcb484928284313804a73d1575231b64babb
Justin Onyango <ojustin446@gmail.com>
2024-03-19 23:13:46 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const hqLocation = 42; 
+    return Math.abs(hqLocation - location);
+}
+
+function distanceFromHqInFeet(location) {
+    const feetPerBlock = 264;
+    return distanceFromHqInBlocks(location) * feetPerBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    return Math.abs(destination - start) * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare = 0;
+
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        fare = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        fare = 25;
+    } else {
+        return 'cannot travel that far';
+    }
+
+    return fare;
+}
+      
